1a5b7f9f255dc69b2718badebbda3764020efed5
labellavita <92790526+Iabellavita@users.noreply.github.com>
2022-04-02 22:30:31 +0300

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,26 +1,26 @@
-import smtplib
-import folium
-import time
-import mimetypes
-import os
-import random
 from pyfiglet import Figlet
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import random
+import smtplib
+import time
+import mimetypes
+import os
 
 
-def send_email(sender: str, password: str, recipient: str, subject=None, file_name=None, template=None, message=None):
-    counter = 0
-
+def send_email(sender: str, password: str, recipient: str, template=None, subject=None, file_name=None, message=None):
     # СОЗДАЕМ ОБЬЕКТ И УКАЗЫВАЕМ СЕРВЕР И ПОРТ ДЛЯ GMAIL.COM
-    # server = smtplib.SMTP("smtp.gmail.com", 587)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
     # СОЗДАЕМ ОБЬЕКТ И УКАЗЫВАЕМ СЕРВЕР И ПОРТ ДЛЯ MAIL.RU
-    server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-    server.set_debuglevel(False)
-    # ЗАПУСКАЕМ ШИФРОВАНИЯ ПО TLS только для GMAIL.COM
-    # server.starttls()
+    # server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
+    # СОЗДАЕМ ОБЬЕКТ И УКАЗЫВАЕМ СЕРВЕР И ПОРТ ДЛЯ YANDEX.RU
+    # server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+    # server.set_debuglevel(False)
+    # ЗАПУСКАЕМ ШИФРОВАНИЯ ПО TLS ТОЛЬКО ДЛЯ GMAIL.COM!!!
+    server.starttls()
+    server.ehlo()
 
     try:
         server.login(sender, password)
@@ -52,57 +52,126 @@ def send_email(sender: str, password: str, recipient: str, subject=None, file_na
             msg.attach(file)
 
         server.sendmail(sender, recipient, msg.as_string())
-
         return True
 
-    except Exception as ex:
-        return ex
+    except:
+        return False
 
 
 def main():
-    preview_text = Figlet(font='doom', width=200)
-    text = preview_text.renderText('E M A I L   S P A M M E R')
-    print(f'\033[31m\033[1m{text}\033[0m')
+    try:
+        with open('emails.txt') as f:
+            emails = f.read()
+        list_emails = emails.split('\n')
 
-    with open('emails.txt') as f:
-        emails = f.readlines()
+    except FileNotFoundError:
+        print(f"\n\033[31m\033[1m[ERROR]\033[0m PLease check if file \033[31m\033[4memails.txt\033[0m is exist")
+        exit()
 
-    sender = input("\033[32m\033[1m[ACTION]\033[0m Input your email: ")
-    password = input("\033[32m\033[1m[ACTION]\033[0m Input your password: ")
-    start = int(input("\033[32m\033[1m[ACTION]\033[0m Input number email-address start (0 or other number) #: "))
+    try:
+        start_script = int(input("Input your \033[32m\033[1mCHOICE\033[0m:\n"
+                                 "[\033[32m\033[1m1\033[0m]---\033[32m\033[1mINFO-TEMPLATE\033[0m\n"
+                                 "[\033[32m\033[1m2\033[0m]---\033[32m\033[1mFAKE-INFO-MOZ\033[0m\n"
+                                 "Please input your choice \033[32m\033[1m(1 or 2)\033[0m: "))
+    except ValueError:
+        print(f"\n\033[31m\033[1m[ERROR]\033[0m Check your input, please!")
+        exit()
 
-    print()
+    if 1 <= start_script <= 2:
 
-    counter = 0
-    for email in emails[start:]:
-        recipient = email.strip()
+        print()
+        sender = input('\033[32m\033[1m[ACTION]\033[0m Input your \033[31m\033[1memail\033[0m: ')
+        password = input('\033[32m\033[1m[ACTION]\033[0m Input \033[31m\033[1mpassword\033[0m: ')
 
-        subject = 'Информация для патриотически настроенных'
-        # subject = input('Input SUBJECT')
-        message = f'Уважаемый гражданин Российской Федерации, прошу ознакомиться с информацией ниже ' \
-                  f'и передать информацию своим коллегам. Спасибо.\n'
-        # message = input('Input your MESSAGE: ')
-        file_name = None
-        # file_name = input('File name: ')
-        template = 'Document.html'
-        # template = input('Select your TEMPLATE: ')
-        # recipient = input('Recipient EMAIL: ')
-        counter += 1
-        time.sleep(random.randint(1, 5))
-        if send_email(sender=sender, password=password, recipient=recipient, subject=subject,
-                      template=template, file_name=file_name, message=message):
-            print(f'#\033[33m\033[1m{start}\033[0m  Message to \033[31m\033[1m{email}\033[0m sent successfully!')
-            start += 1
-        else:
-            print(
-                send_email(sender=sender, password=password, recipient=recipient,
-                           subject=subject, file_name=file_name, message=message))
-        if counter == 499:
-            print(f'[INFO]   Sending stopped, LIMIT is EXHAUSTED. Please try again in a couple of hours.'
-                  f'\nSent \033[31m\033[1m{counter}\033[0m letters. Good work!')
-            break
+        try:
+            start = int(input("\033[32m\033[1m[ACTION]\033[0m Input \033[31m\033[1mnumber email-address\033[0m start"
+                              " \033[31m\033[1m(0 or other number)\033[0m #: "))
+        except ValueError:
+            print(f"\n\033[31m\033[1m[ERROR]\033[0m Check your input, please!")
+            exit()
+        print()
+
+        counter = 0
+        # counter_error = 0
+        for email in list_emails[start:]:
+            recipient = email.replace("\n", "")
+            recipient = email.replace("\t", "")
+            recipient = email.strip()
+
+            if start_script == 2:
+                subject = 'Министерство здравоохранения Российской Федерации'
+                message = f'В СООТВЕТСТВИИ  С РАСПОРЯЖЕНИЕМ МИНИСТРА ЗДРАВООХРАНЕНИЯ РОССИЙСКОЙ ФЕДЕРАЦИИ ' \
+                          f'М.А. МУРАШКО № 62Н ОТ 22 МАРТА 2022 Г.\n\nНемедленно государственным учреждениям, ' \
+                          f'которые имеют в распоряжении сайты и другие средства публикации, опубликовать следующее ' \
+                          f'обращение Министра здравоохранения Российской Федерации к гражданам нашей страны. ' \
+                          f'О публикации уведомить ответом на это письмо с ' \
+                          f'ссылкой на страницу публикации до 25.03.2022г.\n'
+                file_name = 'Обращение Министра здравоохранения Российской Федерации.jpg'
+                template = None
+
+                if send_email(sender=sender, password=password, recipient=recipient, subject=subject,
+                              template=template, file_name=file_name, message=message):
+                    print(
+                        f'\033[33m\033[1m#{start}\033[0m  Message to \033[31m\033[1m{email}\033[0m sent successfully!')
+                    counter += 1
+                    time.sleep(random.randint(2, 3))
+                    start += 1
+                else:
+                    print("\033[33m\033[1mNOT SENT, socket.gaierror!\033[0m")
+                    # counter_error += 1
+                    # if counter_error > 3:
+                    #     print(
+                    #         f'\n\033[31m\033[1m[ERROR]\033[0m Sending STOPPED! PROBLEM WITH SOCKET. Sent \033[31m\033[1m{counter}\033[0m letters.\n'
+                    #         f'\033[31m\033[1m[ERROR]\033[0m Please, try again after a while or change your email-address.\n')
+                    #     break
+
+                if counter == 999:
+                    print(
+                        f'\n\033[32m\033[1m[INFO]\033[0m Sending stopped, LIMIT is EXHAUSTED.\n'
+                        f'Please try again in a couple of hours.\n'
+                        f'Sent \033[31m\033[1m{counter}\033[0m letters. Good work!\n')
+                    break
+
+            if start_script == 1:
+                subject = 'Информация для Патриотически Настроенных'
+                message = f'СРОЧНОЕ ОБРАЩЕНИЕ К ГРАЖДАНАМ РОССИЙСКОЙ ФЕДЕРАЦИИ. ДОКУМЕНТ ОБЯЗАТЕЛЕН ДЛЯ ОЗНАКОМЛЕНИЯ.\n'
+                file_name = None
+                template = 'Document.html'
+
+                if send_email(sender=sender, password=password, recipient=recipient, subject=subject,
+                              template=template, file_name=file_name, message=message):
+                    print(
+                        f'\033[33m\033[1m#{start}\033[0m  Message to \033[31m\033[1m{email}\033[0m sent successfully!')
+                    counter += 1
+                    time.sleep(random.randint(2, 3))
+                    start += 1
+                else:
+                    print("\033[33m\033[1mNOT SENT, socket.gaierror!\033[0m")
+                    # counter_error += 1
+                    # if counter_error > 3:
+                    #     print(
+                    #         f'\n\033[31m\033[1m[ERROR]\033[0m Sending STOPPED! PROBLEM WITH SOCKET. Sent \033[31m\033[1m{counter}\033[0m letters.\n'
+                    #         f'\033[31m\033[1m[ERROR]\033[0m Please, try again after a while or change your email-address.\n')
+                    #     break
+
+                if counter == 999:
+                    print(
+                        f'\n\033[32m\033[1m[INFO]\033[0m Sending stopped, LIMIT FOR THE DAY is EXHAUSTED.\n'
+                        f'Please try again the next day.\n'
+                        f'Sent \033[31m\033[1m{counter}\033[0m letters. Good work!\n')
+                    break
+    else:
+        print(f"\n\033[31m\033[1m[ERROR]\033[0m Check your input, please!\n")
 
 
 if __name__ == "__main__":
-    main()
+    preview_text = Figlet(font='doom', width=200)
+    text = preview_text.renderText('E M A I L    S P A M M E R')
+    print(f'\033[31m\033[1m{text}\033[0m')
+    print("\033[31m\033[1m-\033[0m" * 100)
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\033[31m\033[1m[ERROR]\033[0m PROGRAM STOPPED BY USER\n")
 
